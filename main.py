5c678d8ebe76087ff6f134fe22ffefd53089fb49
Sanskar Pal <96393404+sanskarpal@users.noreply.github.com>
2025-12-02 13:19:08 -0800

    Implement input conversion and error handling
    Added error handling for user input conversion.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Tried to run the file, if you see this successful!")
+def to_int_or_error(s: str) -> int:
+    return int(s)
+
+def main():
+    user_input = input()
+    try:
+        num = to_int_or_error(user_input)
+        print(num * num)
+    except Exception as e:
+        print(f"error: {e}")
+
+if __name__ == "__main__":
+    main()
